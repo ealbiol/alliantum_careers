a9265a1afe291c5347ca430ee5a1c44bdb0f7f86
eduardalbiol <eduard.romero@alliantum.com>
2021-10-20 22:23:01 +0200

    Tailwind functioning

--- gatsby-config.js
+++ gatsby-config.js
@@ -134,5 +134,16 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
 
+
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          // require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+        ],
+      },
+    },
+
   ],
 }
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,6 +3,9 @@ import { Link } from "gatsby"
 
 import Layout from "../components/Layout/index"
 import Seo from "../components/seo"
+//Global:
+// import "../scss/index.scss"
+import "../scss/pagesScss/page-2.scss";
 
 const SecondPage = () => (
   <Layout>
@@ -10,6 +13,10 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
 
   </Layout>
 )
