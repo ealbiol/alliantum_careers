144da8ecabe0881118b40a52e6ade1b4ed522e22
all-ei <eder.iraizoz@alliantum.com>
2021-10-27 17:22:44 +0200

    almost stylised

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { getAllHeaderAndFooterSections } from "../../data/data"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { StaticImage } from "gatsby-plugin-image"
 
 
 export default function Header({ siteTitle }) {
@@ -17,12 +18,20 @@ export default function Header({ siteTitle }) {
   }, [])
 
   return (
-    <header style={{ background: `rebeccapurple`, marginBottom: `1.45rem` }}>
+    <header style={{ marginBottom: `1.45rem` }}>
 
-      <div style={{ margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem` }}>
+      <div>
 
-        <div>
-          <Link to="/" >Logo Alliantum</Link>
+        <div class="flex flex-row justify-between container mx-auto py-3 bb bg-red">
+          <Link to="/" > 
+            <StaticImage
+                src="../../images/alliantum-brand/alliantum-logo.png"
+                alt="Alliantum"
+                placeholder="blurred"
+                layout="fixed"
+                height={30}
+            />
+          </Link>
           <div>
             {
               headerUnite.map((unite, index) => {
