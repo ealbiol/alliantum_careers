1ad007a9479b6b258964a348bf64df4465d91f33
eduardalbiol <eduard.romero@alliantum.com>
2021-11-02 12:53:12 +0100

    Post method Fetch added on 'Notify Me' Button

--- src/components/NotifyMeButton/index.js
+++ src/components/NotifyMeButton/index.js
@@ -1,10 +1,47 @@
 import * as React from "react"
+import { sendInBlueApiKey } from "../../../credentials";
 
-export function NotifyMeButton({ style }) {
+export function NotifyMeButton() {
+
+    const [userEmail, setUserEmail] = React.useState("")
+    const URL = "https://api.sendinblue.com/v3/contacts"
+
+    function handleUserEmail(e) {
+        e.preventDefault();
+        console.log("User email:--->", userEmail);
+
+        const options = {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "api-key": sendInBlueApiKey,
+            },
+            body: JSON.stringify({ "email": userEmail })
+        };
+
+        fetch(URL, options)
+            .then(response => response.json())
+            .then(data => console.log("Added email:--->", data))
+            .catch(err => console.error(err));
+
+        console.log(options);
+    }
+
+
     return (
-        <button style={style} >
-            <span>(Bell Icon){" "}</span>
-            <span>Notify me on new jobs</span>
-        </button>
+        <form onSubmit={handleUserEmail} >
+            <input
+                type="email"
+                name="Notify Me"
+                placeholder="email"
+                onChange={(e) => setUserEmail(e.currentTarget.value)}
+                required
+            />
+            <button type="submit"   >
+                <span>(Bell Icon){" "}</span>
+                <span>Notify me on new jobs</span>
+            </button>
+        </form>
     )
 }
