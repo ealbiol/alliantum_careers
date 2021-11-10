c8e60158caaebd790cbe6b7edbf63933745a3fda
eduardalbiol <eduard.romero@alliantum.com>
2021-11-10 11:16:50 +0100

    Changing icons 5 and 6 from 'your-benefits-photos' images folder.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { getAllHeaderAndFooterSections, getAllAdresses } from "../../data/data"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
+import AlliantumLogo from "../../images/header-photos/Image1.svg"
 
 
 export default function Footer() {
@@ -25,7 +26,9 @@ export default function Footer() {
         <footer style={{ background: `rebeccapurple`, marginBottom: `1.45rem` }}>
             <div style={{ margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem` }}>
 
-                <Link to="/" >Logo Alliantum</Link>
+                <Link to="/" >
+                    <AlliantumLogo />
+                </Link>
 
                 <AnchorLink to="/#jobs-table">The jobs</AnchorLink>
                 <div style={{ border: "1px solid black" }} >
