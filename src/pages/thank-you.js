eef949e96b9bc01a894f747b30e8be713de6e85f
eduardalbiol <eduard.romero@alliantum.com>
2021-11-15 12:01:22 +0100

    'thank-you.js' Page created.

---
+++ src/pages/thank-you.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+
+export function ThankYou() {
+    return (
+        <Layout>
+            <div>Thank You!</div>
+        </Layout>
+    )
+}
+
+export default ThankYou
