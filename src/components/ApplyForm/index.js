4fd9eb8eb1e63c06233555fc6d46327411447bf3
eduardalbiol <eduard.romero@alliantum.com>
2021-11-15 10:56:40 +0100

    Amending ApplyForm.js form inputs

--- src/components/ApplyForm/index.js
+++ src/components/ApplyForm/index.js
@@ -16,7 +16,7 @@ export function ApplyForm() {
 
         <form name="contact" method="POST" data-netlify="true" style={{ border: "1px solid black" }} >
             <input type="hidden" name="form-name" value="contact" />
-            {
+            {/* {
                 formFields.map((field, index) => {
                     return (
                         <label key={index} >
@@ -24,7 +24,12 @@ export function ApplyForm() {
                         </label>
                     )
                 })
-            }
+            } */}
+            <input type="text" name="Applicant Name" placeholder="Your name" required />
+            <input type="text" name="Applicant Last Name" placeholder="Your last name" required />
+            <textarea name="subject" id="subject" rows="5" placeholder="Tell us about your talents" required />
+            <input type="email" name="Applicant Email" placeholder="Your email" required />
+            <input type="file" name="Applicant CV" placeholder="Your CV" required />
             <button type="submit" >Apply for this job</button>
         </form>
 
