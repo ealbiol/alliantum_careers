d875c7e1bd05e749937db1d7b7021e721af209bd
eduardalbiol <eduard.romero@alliantum.com>
2021-11-17 12:54:12 +0100

    testing

---
+++ src/pages/check-your-inbox.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+import Seo from "../components/seo"
+
+function CheckYourInbox() {
+    return (
+        <Layout>
+            <Seo title="CheckYourInbox" />
+            <h1>CheckYourInbox Content</h1>
+
+
+            <div className="flex" >
+                <div className="w-6/12" >Lorem</div>
+                <div className="w-6/12" >Lorem</div>
+            </div>
+
+
+        </Layout>
+    )
+}
+
+export default CheckYourInbox
