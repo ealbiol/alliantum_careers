2f38e4d251d7374f3714faf553198c704e240944
all-ei <eder.iraizoz@alliantum.com>
2021-11-18 14:56:49 +0100

    error fix

--- src/components/ValueCard/index.js
+++ src/components/ValueCard/index.js
@@ -1,20 +1,18 @@
+
 import * as React from "react"
 import { ValuePhoto } from "../ValuePhoto"
-
 export function ValueCard({ value }) {
-
     console.log("Value inside ValueCard:--->", value);
-
     return (
-
-        <div>
-            <ValuePhoto className="w-100" value={value} />
-            <div className="p-4">
-                <h3 className="text-md text-uppercase text-cornflower-blue uppercase">{value?.title}</h3>
-                <p className="text-sm">{value?.content}</p>
+        value ?
+            <div>
+                <ValuePhoto className="w-100" value={value} />
+                <div className="p-4">
+                    <h3 className="text-uppercase text-uppercase text-cornflower-blue uppercase">{value.title}</h3>
+                    <p className="text-sm">{value.content}</p>
+                </div>
             </div>
             :
             null
-            )
-        </div>
+    )
 }
