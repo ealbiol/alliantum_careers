5c9e76c9cbe99bd4233ee1db99b2ab5a8321c54e
eduardalbiol <eduard.romero@alliantum.com>
2021-11-24 11:51:52 +0100

    Applying styles in job-offer template page.

--- src/templates/job-offer.js
+++ src/templates/job-offer.js
@@ -31,19 +31,26 @@ const JobOfferTemplate = ({ data, location }) => {
           <div className="w-7/12"  >
             <div >
 
-              <div className="flex" >
+              <div className="flex mb-6" >
                 <div className="pill" >{jobOffer.frontmatter.department}</div>
               </div>
 
-              <div className="pretitle text-loblolly-600">WORK WITH US AS</div>
-              <div className="title text-md w-7/12" >{jobOffer.frontmatter.title}</div>
+              <div className="pretitle text-loblolly-600" >WORK WITH US AS</div>
+              <div className="title text-md w-7/12 mt-1" >{jobOffer.frontmatter.title}</div>
 
-              <div className="pretitle text-loblolly-600" >MINIMUM EXPERIENCE</div>
-              <div>{jobOffer.frontmatter.experience}</div>
+              <div className="flex" >
+                <div className="flex-grow" >
+                  <div className="pretitle text-loblolly-600" >MINIMUM EXPERIENCE</div>
+                  <div>{jobOffer.frontmatter.experience}</div>
+                </div>
+                <div className="flex-grow items-end" >
+                  <div className="pretitle text-loblolly-600" >LANGUAGES REQUIRED</div>
+                  <div>{jobOffer.frontmatter.languageRequired1}</div>
+                  <div>{jobOffer.frontmatter.languageRequired2}</div>
+                </div>
 
-              <div className="pretitle text-loblolly-600" >LANGUAGES REQUIRED</div>
-              <div>{jobOffer.frontmatter.languageRequired1}</div>
-              <div>{jobOffer.frontmatter.languageRequired2}</div>
+              </div>
+
 
             </div>
 
@@ -52,7 +59,7 @@ const JobOfferTemplate = ({ data, location }) => {
               itemProp="articleBody"
             />
 
-            <div >
+            <div className="mb-7" >
               <div className="pretitle text-loblolly-600" >WHAT WE NEED RIGHT NOW</div>
               <div>
                 <AllJobOffersList />
@@ -61,7 +68,7 @@ const JobOfferTemplate = ({ data, location }) => {
 
           </div>
 
-          <div className="w-5/12 flex justify-end" >
+          <div className="w-5/12 flex justify-end mt-7" >
             <div className="max-w-md flex-grow fixed" >
               <ApplyForm />
             </div>
@@ -69,13 +76,10 @@ const JobOfferTemplate = ({ data, location }) => {
         </div>
 
 
+      </div>
 
-
-        <div className="z-10 relative" style={{ border: "4px solid green" }} >
-          <YourBenefits />
-        </div>
-
-
+      <div className="z-10 relative bg-festival" >
+        <YourBenefits />
       </div>
 
     </Layout>
