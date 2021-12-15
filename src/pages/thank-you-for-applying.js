6958d0d0334d825ba25e4b9bd74818b0b1b1cbe1
eduardalbiol <eduard.romero@alliantum.com>
2021-12-15 11:31:34 +0100

    Adding thank-you-for-applying.js page

--- src/pages/thank-you-for-applying.js
+++ src/pages/thank-you-for-applying.js
@@ -1,22 +1,11 @@
 import * as React from "react"
 import Layout from "../components/Layout"
-import Seo from "../components/seo"
-import { AnchorLink } from "gatsby-plugin-anchor-links";
 
-export function ThankYouForApplying() {
+
+function ThankYouForApplying() {
     return (
-        <Layout>
-            <Seo title="Thank You for applying" />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <h1>Thank you for applying!</h1>
-            <AnchorLink to="/" >
-                <button className="btn btn-primary">Go back home</button>
-            </AnchorLink>
-        </Layout>
-
+        <Layout>Thank you for applying</Layout>
     )
 }
+
+export default ThankYouForApplying
