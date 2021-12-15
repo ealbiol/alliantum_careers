75aa10dfcfd668629c167ba3b8daa45793888ec4
eduardalbiol <eduard.romero@alliantum.com>
2021-12-15 12:07:56 +0100

    Adding style to thank-you-for-applying.js Page.

--- src/pages/thank-you-for-applying.js
+++ src/pages/thank-you-for-applying.js
@@ -1,10 +1,22 @@
 import * as React from "react"
 import Layout from "../components/Layout"
+import Seo from "../components/seo"
+import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 
 function ThankYouForApplying() {
     return (
-        <Layout>Thank you for applying</Layout>
+        <Layout>
+            <Seo title="Thank You for applying" />
+            <div className="container mx-auto my-6 h-screen flex flex-col justify-center ">
+                <div>
+                    <h1 className="text-lg">Thank you for applying!</h1>
+                    <AnchorLink to="/" >
+                        <button className="btn btn-primary">Go back home</button>
+                    </AnchorLink>
+                </div>
+            </div>
+        </Layout>
     )
 }
 
