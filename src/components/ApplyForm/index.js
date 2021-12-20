9f362c86964030db558fa5c66a9d2a8b827bebcb
eduardalbiol <eduard.romero@alliantum.com>
2021-12-20 16:36:31 +0100

    Testing input file.

--- src/components/ApplyForm/index.js
+++ src/components/ApplyForm/index.js
@@ -57,7 +57,6 @@ export function ApplyForm({ jobDepartment, jobTitle }) {
                         </label>
                 }
                 <input ref={item} type="file" id="uploadCV" name="Applicant CV" placeholder="Your CV" className="flex-grow" onChange={handleFileInput} required />
-                <input type="file" placeholder="test" />
 
             </div>
             <div className="flex mb-2">
